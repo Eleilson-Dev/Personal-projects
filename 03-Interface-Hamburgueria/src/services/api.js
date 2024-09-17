ec067bc5cfe71046e9e6550e8933b025291b8a68
Mxt@DEV <maxta.gamer@gmail.com>
2024-09-17 11:02:20 -0300

    update

--- 03-Interface-Hamburgueria/src/services/api.js
+++ 03-Interface-Hamburgueria/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://api-hamburgueria-00xc.onrender.com/',
-  timeout: 8000,
+  baseURL: 'https://api-hamburgueria-umn6.onrender.com/',
+  timeout: 60 * 1000,
 });
